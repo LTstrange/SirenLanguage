64ff65bb3b5cdad8d73e6f69c59dce2521310927
LTstrange <835422774@qq.com>
2023-07-02 01:07:13 +0800

    add comment for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ pub use parser::SirenParser;
 mod interpreter;
 pub use interpreter::Environment;
 
+// run a line of code on the given environment, and return the result
 pub fn run(env: &mut Environment, input: &str) -> Result<Option<i64>, String> {
-    let stmt = SirenParser::parse(input)?;
-    env.eval(stmt)
+    let stmt = SirenParser::parse(input)?; // parse the input
+    env.eval(stmt) // evaluate the input
 }
