9ab331d89e4148b70c28945871f85c6a536ec7c5
LTstrange <835422774@qq.com>
2023-07-02 01:09:44 +0800

    add comment for parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -7,10 +7,11 @@ pub mod statement;
 pub struct SirenParser;
 impl SirenParser {
     pub fn parse(_input: &str) -> Result<Statement, String> {
+        // use statement parser to parse one line code
         match statement(_input) {
-            Ok((i, statement)) if i.is_empty() => Ok(statement),
+            Ok((i, statement)) if i.is_empty() => Ok(statement), // success only if the input has reached the end
             Ok((_, _)) => Err(String::from("Invalid input")),
-            Err(err) => Err(format!("{:?}", err)),
+            Err(err) => Err(format!("{:?}", err)), // return the parse error
         }
     }
 }
