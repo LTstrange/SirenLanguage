de3f2f8aa02ebcbc30296996250f0e22fe215069
LTstrange <835422774@qq.com>
2023-07-02 01:06:09 +0800

    add comment for main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,29 @@ use siren_language::{run, Environment};
 use std::io::{self, Write};
 
 fn main() {
+    // Create a new running environment
     let mut env = Environment::new();
     // repl
     loop {
         print!("> ");
         io::stdout().flush().unwrap();
+
+        // get user input
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
         let input = input.trim();
+
+        // parse and run input
         match input {
             "" => continue,
             "quit" | "q" => break,
+            // run input on the environment
             input => match run(&mut env, input) {
                 Ok(output) => match output {
-                    Some(number) => println!("{}", number),
-                    None => continue,
+                    Some(number) => println!("{}", number), // print the result
+                    None => continue, // print nothing because of let statement
                 },
-                Err(msg) => println!("{}", format!("Error: {}", msg).red()),
+                Err(msg) => println!("{}", format!("Error: {}", msg).red()), // print error
             },
         }
     }
