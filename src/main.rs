164d1e9daddd3d137fd92b906078afa495515eb7
LTstrange <835422774@qq.com>
2023-07-02 15:13:27 +0800

    add Usage for help

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 use colored::Colorize;
 use siren_language::{run, Environment};
-use std::io::{self, Write};
+use std::io::{self, Read, Write};
 
 fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
     // Create a new running environment
     let mut env = Environment::new();
+
+    if args.len() == 1 {
+        repl(&mut env);
+    } else if args.len() == 2 && args[1].ends_with(".siren") {
+        // file interprete
+        let mut file = std::fs::File::open(&args[1]).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        println!("{}", contents);
+    } else {
+        println!("Usage:");
+        println!("    siren              : repl");
+        println!("    siren <file>.siren : interpret file");
+    }
+}
+
+fn repl(env: &mut Environment) {
     // repl
     loop {
         print!("> ");
@@ -20,7 +38,7 @@ fn main() {
             "" => continue,
             "quit" | "q" => break,
             // run input on the environment
-            input => match run(&mut env, input) {
+            input => match run(env, input) {
                 Ok(output) => match output {
                     Some(number) => println!("{}", number), // print the result
                     None => continue, // print nothing because of let statement
