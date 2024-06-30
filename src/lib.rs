ce2e0742ada325ce253f287b76ef8281a9e43091
LTstrange <835422774@qq.com>
2024-06-30 17:20:19 +0800

    remove old, uncompatable code

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,28 @@
+// mod interpreter;
 mod parser;
-pub use parser::SirenParser;
-mod interpreter;
-pub use interpreter::Evaluator;
-use interpreter::Value;
-mod lexer;
 
-// run a line of code on the given environment, and return the result
-pub fn run(evaluator: &mut Evaluator, input: &str) -> Result<Value, String> {
-    let stmt = SirenParser::parse_line(input)?; // parse the input
-    evaluator.eval(&stmt) // evaluate the input
-}
+// pub use interpreter::Evaluator;
+// use interpreter::Value;
 
-// run a file of code on the given environment, and return the result
-pub fn run_file(evaluator: &mut Evaluator, content: String) -> Result<(), String> {
-    let program = SirenParser::parse_file(&content)?; // parse the input
-    for stmt in program.0 {
-        evaluator.eval(&stmt)?; // evaluate the input
+// // run a line of code on the given environment, and return the result
+// pub fn run(evaluator: &mut Evaluator, input: &str) -> Result<Value, String> {
+//     let stmt = parser::parse_line(input)?; // parse the input
+//     evaluator.eval(&stmt) // evaluate the input
+// }
+
+// // run a file of code on the given environment, and return the result
+// pub fn run_file(evaluator: &mut Evaluator, content: String) -> Result<(), String> {
+//     let program = parser::parse_file(&content)?; // parse the input
+//     for stmt in program.0 {
+//         evaluator.eval(&stmt)?; // evaluate the input
+//     }
+//     Ok(())
+// }
+
+pub fn run_file(input: &str) -> Result<(), String> {
+    let program = parser::parse_file(input).map_err(|e| format!("Parse error: {}", e))?;
+    for item in program.0 {
+        println!("{:?}", item);
     }
     Ok(())
 }
