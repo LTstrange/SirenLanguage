acb7f77d1bd5875bf41e7f0f10a2d2dd72353895
LTstrange <835422774@qq.com>
2024-06-30 17:18:01 +0800

    rebuild parser
    using pest

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,179 +1,39 @@
-use std::fmt::{self, Debug, Formatter};
+#[derive(Debug)]
+pub struct Program<'a>(pub Vec<Item<'a>>);
 
-pub struct Program(pub BlockStmt);
-
-pub type BlockStmt = Vec<Statement>;
-
-#[derive(PartialEq, Clone)]
-pub enum Statement {
-    Let { name: String, value: Expr },
-    Expr(Expr),
-    Set { name: String, value: Expr },
-    Return(Expr),
+#[derive(Debug)]
+pub enum Item<'a> {
+    DefItem { ident: &'a str, expr: Expr<'a> },
 }
 
-impl Debug for Statement {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Statement::Let { name, value } => write!(f, "let {} = {:?}", name, value),
-            Statement::Expr(expr) => write!(f, "expr {:?}", expr),
-            Statement::Set { name, value } => write!(f, "set {} = {:?}", name, value),
-            Statement::Return(ret) => write!(f, "return {:?}", ret),
-        }
-    }
-}
-
-#[derive(PartialEq, Clone)]
-pub enum Literal {
-    Int(i64),
-    Bool(bool),
-    // Str(String),
-}
-impl Debug for Literal {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Literal::Int(i) => write!(f, "{}", i),
-            Literal::Bool(b) => write!(f, "{}", b),
-            // Literal::Str(s) => write!(f, "{}", s),
-        }
-    }
-}
-
-#[derive(PartialEq, Clone)]
-pub enum Expr {
-    Ident(String),
+#[derive(Debug)]
+pub enum Expr<'a> {
+    Ident(&'a str),
     Literal(Literal),
-    UnExpr(Prefix, Box<Expr>),
-    BinExpr(Box<Expr>, Infix, Box<Expr>),
-    Function {
-        params: Vec<String>,
-        body: BlockStmt,
-    },
+    BinOp(Box<Expr<'a>>, Infix, Box<Expr<'a>>),
+    Fn(Function<'a>),
     Call {
-        func: Box<Expr>,
-        args: Vec<Expr>,
-    },
-    Index {
-        arr: Box<Expr>,
-        index: Box<Expr>,
-    },
-    If {
-        cond: Box<Expr>,
-        then: BlockStmt,
-        els: Option<BlockStmt>,
+        func: Box<Function<'a>>,
+        args: Vec<Expr<'a>>,
     },
 }
 
-impl Debug for Expr {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Expr::Ident(name) => write!(f, "{}", name),
-            Expr::Literal(l) => write!(f, "{:?}", l),
-            Expr::UnExpr(op, e) => write!(f, "({:?}{:?})", op, e),
-            Expr::BinExpr(l, op, r) => write!(f, "({:?} {:?} {:?})", l, op, r),
-            Expr::Function { params, body } => {
-                write!(
-                    f,
-                    "fn({}) {{ {}}}",
-                    params.join(", "),
-                    body.iter()
-                        .map(|stmt| format!("{:?}; ", stmt))
-                        .collect::<String>()
-                )
-            }
-            Expr::Call { func, args } => write!(
-                f,
-                "{:?}({})",
-                func,
-                args.iter()
-                    .map(|expr| format!("{:?}", expr))
-                    .collect::<Vec<String>>()
-                    .join(", ")
-            ),
-            Expr::Index { .. } => todo!(),
-            Expr::If { cond, then, els } => match els {
-                Some(els_block) => write!(
-                    f,
-                    "if {:?} {{ {}}} else {{ {}}}",
-                    cond,
-                    then.iter()
-                        .map(|stmt| format!("{:?}; ", stmt))
-                        .collect::<String>(),
-                    els_block
-                        .iter()
-                        .map(|stmt| format!("{:?}; ", stmt))
-                        .collect::<String>()
-                ),
-                None => write!(
-                    f,
-                    "if {:?} {{ {}}}",
-                    cond,
-                    then.iter()
-                        .map(|stmt| format!("{:?}; ", stmt))
-                        .collect::<String>()
-                ),
-            },
-        }
-    }
+#[derive(Debug)]
+pub enum Literal {
+    Number(f32),
+    Boolean(bool),
 }
 
-#[derive(PartialEq, Clone)]
-pub enum Prefix {
-    Minus,
-    Not,
-}
-
-impl Debug for Prefix {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match *self {
-            Prefix::Minus => write!(f, "-"),
-            Prefix::Not => write!(f, "!"),
-        }
-    }
-}
-
-#[derive(PartialEq, Clone)]
+#[derive(Debug)]
 pub enum Infix {
     Add,
     Sub,
     Mul,
     Div,
-    Eql,
-    Neq,
-    Lt,
-    Gt,
-    Lte,
-    Gte,
-    Call,
-    Index,
 }
 
-impl Debug for Infix {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Infix::Add => write!(f, "+"),
-            Infix::Sub => write!(f, "-"),
-            Infix::Mul => write!(f, "*"),
-            Infix::Div => write!(f, "/"),
-            Infix::Eql => write!(f, "=="),
-            Infix::Neq => write!(f, "!="),
-            Infix::Lt => write!(f, "<"),
-            Infix::Gt => write!(f, ">"),
-            Infix::Lte => write!(f, "<="),
-            Infix::Gte => write!(f, ">="),
-            Infix::Call => unreachable!(),
-            Infix::Index => unreachable!(),
-        }
-    }
-}
-
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-pub enum Prec {
-    Lowest,
-    Equals,
-    LessGreater,
-    Sum,
-    Product,
-    Call,
-    Index,
+#[derive(Debug)]
+pub struct Function<'a> {
+    params: Vec<String>,
+    body: Vec<Item<'a>>, // todo: change to Statements
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,33 +1,81 @@
-pub mod parse_func;
-
 mod ast;
-pub use ast::*;
-use nom::InputLength;
-pub use parse_func::*;
 
-use crate::lexer::{lexer, Tokens};
+use ast::*;
+use pest::{
+    error::Error,
+    iterators::{Pair, Pairs},
+    pratt_parser::{Assoc, Op, PrattParser},
+    Parser,
+};
+use pest_derive::Parser;
 
-pub struct SirenParser;
-impl SirenParser {
-    pub fn parse_file(input: &str) -> Result<Program, String> {
-        let lexer = lexer(input);
-        let tokens = Tokens::new(&lexer);
+#[derive(Parser)]
+#[grammar = "parser/grammar.pest"]
+struct SirenParser;
 
-        match program(tokens) {
-            Ok((i, stmts)) if i.input_len() == 0 => Ok(stmts),
-            Ok((i, _)) => Err(format!("Here has something left to parse: {:?}", i)),
-            Err(err) => Err(format!("{:?}", err)), // return the parse error
+fn build_pratt_parser() -> PrattParser<Rule> {
+    PrattParser::new()
+        .op(Op::infix(Rule::add, Assoc::Left) | Op::infix(Rule::sub, Assoc::Left))
+        .op(Op::infix(Rule::mul, Assoc::Left) | Op::infix(Rule::div, Assoc::Left))
+        .op(Op::prefix(Rule::neg))
+        .op(Op::postfix(Rule::call))
+}
+
+fn pratt_parse<'a>(expr: Pairs<'a, Rule>, pratt: &PrattParser<Rule>) -> Expr<'a> {
+    pratt
+        .map_primary(|primary| match primary.as_rule() {
+            Rule::ident => Expr::Ident(primary.as_str()),
+            Rule::number => Expr::Literal(Literal::Number(primary.as_str().parse().unwrap())),
+            Rule::boolean => Expr::Literal(Literal::Boolean(primary.as_str().parse().unwrap())),
+            p => unreachable!("get unexpected primary in pratt: {p:?}"),
+        })
+        .map_infix(|lhs, op, rhs| match op.as_rule() {
+            Rule::add => Expr::BinOp(Box::new(lhs), Infix::Add, Box::new(rhs)),
+            Rule::sub => Expr::BinOp(Box::new(lhs), Infix::Sub, Box::new(rhs)),
+            Rule::mul => Expr::BinOp(Box::new(lhs), Infix::Mul, Box::new(rhs)),
+            Rule::div => Expr::BinOp(Box::new(lhs), Infix::Div, Box::new(rhs)),
+            _ => unreachable!("get unexpected infix operator in pratt: {op:?}"),
+        })
+        .parse(expr)
+}
+
+fn parse_item<'a>(item: Pair<'a, Rule>, pratt: &PrattParser<Rule>) -> Option<Item<'a>> {
+    match item.as_rule() {
+        Rule::let_stmt => {
+            let mut pairs = item.into_inner();
+            let ident = pairs.next().unwrap().as_str();
+            let expr = pratt_parse(pairs.next().unwrap().into_inner(), pratt);
+            Some(Item::DefItem { ident, expr })
         }
+        Rule::EOI => None,
+        _ => unreachable!(),
     }
+}
 
-    pub fn parse_line(input: &str) -> Result<Statement, String> {
-        let lexer = lexer(input);
-        let tokens = Tokens::new(&lexer);
-        // use statement parser to parse one line code
-        match statement(tokens) {
-            Ok((i, statement)) if i.input_len() == 0 => Ok(statement), // success only if the input has reached the end
-            Ok((i, _)) => Err(format!("Invalid input; rest {:?}", i)),
-            Err(err) => Err(format!("{:?}", err)), // return the parse error
-        }
+pub fn parse_file(input: &str) -> Result<Program, Error<Rule>> {
+    let pratt = build_pratt_parser();
+    let items: Vec<Item> = SirenParser::parse(Rule::program, input)?
+        .inspect(|r| println!("{r}"))
+        .filter_map(|p| parse_item(p, &pratt))
+        .collect();
+
+    Ok(Program(items))
+}
+
+pub fn parse_line(input: &str) -> Result<Option<Item>, Error<Rule>> {
+    let pratt = build_pratt_parser();
+    SirenParser::parse(Rule::item, input).map(|mut pairs| parse_item(pairs.next().unwrap(), &pratt))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let file = r#"let main = 42 + 1;"#;
+
+        let p = parse_file(file).unwrap();
+        println!("{:#?}", p);
     }
 }
