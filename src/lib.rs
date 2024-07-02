7fb6025d7a499c9d62243ab99229e370e49f4796
LTstrange <835422774@qq.com>
2024-07-03 01:45:19 +0800

    build up interp steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
-// mod interpreter;
 mod parser;
 mod stack_vm;
 
+use parser::*;
+use stack_vm::*;
+
 pub fn run_file(input: &str) -> Result<(), String> {
     let program = parser::parse_file(input)?;
-    for item in program.0 {
-        println!("{}", item);
-    }
+    println!("{}", program);
+    let code = compile(program).unwrap();
+    VM::new(&code).run();
     Ok(())
 }
 
