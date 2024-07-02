f1017019831a3355f5f940247033415cec2efe7b
LTstrange <835422774@qq.com>
2024-07-03 02:34:17 +0800

    add test

--- src/stack_vm/mod.rs
+++ src/stack_vm/mod.rs
@@ -22,7 +22,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_run() {
+    fn test_simple_run() {
         let mut chunk = Chunk::new();
         let c = chunk.add_constant(Value::Number(1.2));
         chunk.add_inst(Inst::Const(c));
@@ -30,6 +30,26 @@ mod tests {
         chunk.add_inst(Inst::Ret);
         let mut vm = VM::new(&chunk);
 
+        match vm.run() {
+            Ok(v) => println!("{}", v),
+            Err(_) => todo!(),
+        }
+    }
+
+    #[test]
+    fn test_simple_expression() {
+        let mut chunk = Chunk::new();
+        let a = chunk.add_constant(Value::Number(1.2));
+        let b = chunk.add_constant(Value::Number(3.4));
+        let c = chunk.add_constant(Value::Number(5.6));
+        chunk.add_inst(Inst::Const(a));
+        chunk.add_inst(Inst::Const(b));
+        chunk.add_inst(Inst::Add);
+        chunk.add_inst(Inst::Const(c));
+        chunk.add_inst(Inst::Div);
+        chunk.add_inst(Inst::Neg);
+        chunk.add_inst(Inst::Ret);
+        let mut vm = VM::new(&chunk);
         match vm.run() {
             Ok(v) => println!("{}", v),
             Err(_) => todo!(),
