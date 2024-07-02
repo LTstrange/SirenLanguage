4c3adcb9aa5f272bf144cb953d559702f2742e3f
LTstrange <835422774@qq.com>
2024-07-03 00:30:41 +0800

    use display trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,15 @@ mod parser;
 pub fn run_file(input: &str) -> Result<(), String> {
     let program = parser::parse_file(input)?;
     for item in program.0 {
-        println!("{:?}", item);
+        println!("{}", item);
     }
     Ok(())
 }
 
 pub fn run_line(input: &str) -> Result<(), String> {
     let item = parser::parse_line(input)?;
-    println!("{:?}", item);
+    if let Some(item) = item {
+        println!("{}", item);
+    }
     Ok(())
 }
