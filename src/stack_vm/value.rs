ec0da819c85f853c5a89d71350ae06795a68ca48
LTstrange <835422774@qq.com>
2024-07-05 21:19:55 +0800

    add new value types
    string and unit

--- src/stack_vm/value.rs
+++ src/stack_vm/value.rs
@@ -3,12 +3,16 @@ use std::fmt::Display;
 #[derive(Clone)]
 pub enum Value {
     Number(f32),
+    String(String),
+    Unit,
 }
 
 impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Value::Number(n) => write!(f, "{}", n),
+            Value::String(s) => write!(f, "{}", s),
+            Value::Unit => write!(f, "()"),
         }
     }
 }
